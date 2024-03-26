5d70c41c6b83ef85aa83a8dcebb7bda64ae1d90a
smooooky <75350537+smooooky@users.noreply.github.com>
2024-03-26 11:14:01 -0400

    style: removed comment

--- comet_backend/accounts/models.py
+++ comet_backend/accounts/models.py
@@ -1,6 +1,5 @@
 from django.db import models
 from django.contrib.postgres.fields import ArrayField
-# Create your models here.
 
 class Account(models.Model):
     id = models.CharField(max_length=30,primary_key=True)
