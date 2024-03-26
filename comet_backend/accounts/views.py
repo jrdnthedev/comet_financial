345f3096da052a255682bdaf5b752234bf52c9ef
smooooky <75350537+smooooky@users.noreply.github.com>
2024-03-25 23:02:45 -0400

    refactor: updated dummy accounts balance value

--- comet_backend/accounts/views.py
+++ comet_backend/accounts/views.py
@@ -3,7 +3,7 @@ from django.http import JsonResponse
 from .models import Account
 
 def index(request):
-    accounts = [{"id": 1, "type": 'savings', "balance": 100000, "transactions": [123,3442,4234,45354,65,543,233,34]},{"id": 2, "type": 'checking', "balance": 45000, "transactions": [123,3442,4234,45354,65,543,233,34]},{"id": 3, "type": 'tsfa', "balance": 10400, "transactions": [123,3442,4234,45354,65,543,233,34]}]
+    accounts = [{"id": 1, "type": 'savings', "balance": 100000, "transactions": [123,3442,4234,45354,65,543,233,34]},{"id": 2, "type": 'checking', "balance": 100000, "transactions": [123,3442,4234,45354,65,543,233,34]},{"id": 3, "type": 'tsfa', "balance": 100000, "transactions": [123,3442,4234,45354,65,543,233,34]}]
     # data = [{"id": account.id, "type": account.type, "balance": account.balance, "transactions": account.transactions} for account in accounts]
     return JsonResponse(accounts,safe=False)
 
