f1c845424766913906fea0c28a4612fa8d438b09
smooooky <75350537+smooooky@users.noreply.github.com>
2024-03-28 00:09:54 -0400

    refactor: added token key to environment variables

--- comet_backend/login/views.py
+++ comet_backend/login/views.py
@@ -3,14 +3,18 @@ from django.db import models
 from django.http import JsonResponse
 from django.views.decorators.csrf import csrf_exempt
 import jwt
+import os
+
+
 
 @csrf_exempt
 def login(request):
     if request.method == 'POST':
         data = json.loads(request.body)
+        token_key = os.environ.get('TOKEN_KEY')
         # Authenticate user, e.g., check username and password
         if data.get('username') == 'admin' and data.get('password') == 'admin':
-            token = jwt.encode({'username': 'admin'}, 'KEY', algorithm='HS256')
+            token = jwt.encode({'username': 'admin'}, str(token_key), algorithm='HS256')
             return JsonResponse({'token': token}, status=200)
         else:
             return JsonResponse({'message': 'Invalid credentials'}, status=401)
