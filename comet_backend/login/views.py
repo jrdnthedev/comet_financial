eb41224634391250e707a53d2e9063fd90eee6ba
smooooky <75350537+smooooky@users.noreply.github.com>
2024-03-27 21:30:40 -0400

    refactor:updated key value

--- comet_backend/login/views.py
+++ comet_backend/login/views.py
@@ -10,7 +10,7 @@ def login(request):
         data = json.loads(request.body)
         # Authenticate user, e.g., check username and password
         if data.get('username') == 'admin' and data.get('password') == 'admin':
-            token = jwt.encode({'username': 'admin'}, '8efd0652d178750527e69a1a7c4ee830aa9ac3288c15465097297963b8d34bc8', algorithm='HS256')
+            token = jwt.encode({'username': 'admin'}, 'KEY', algorithm='HS256')
             return JsonResponse({'token': token}, status=200)
         else:
             return JsonResponse({'message': 'Invalid credentials'}, status=401)
