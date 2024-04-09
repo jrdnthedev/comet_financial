4f47d7e0dde7d2f64d1b6889c9e3356013a74350
smooooky <75350537+smooooky@users.noreply.github.com>
2024-04-09 17:47:37 -0400

    refactor: updated styles

--- comet_frontend/app/components/accounts/accounts.tsx
+++ comet_frontend/app/components/accounts/accounts.tsx
@@ -4,37 +4,45 @@ import { useAppStore } from "@/app/lib/hooks";
 import changeToCurrency from "@/app/utils/changeNumberToCurrencyUtil";
 
 export default function Accounts() {
-  const [accounts, setAccounts] = useState<Account[]>([]);
+  // const [accounts, setAccounts] = useState<Account[]>([]);
   const store = useAppStore().getState();
   const account: Account[] = store.userData.accounts;
 
-  useEffect(() => {
-    const fetchData = () => {
-      setAccounts(account);
-    };
-    fetchData();
-  }, [account]);
+  // useEffect(() => {
+  //   const fetchData = () => {
+  //     setAccounts(account);
+  //   };
+  //   fetchData();
+  // }, [account]);
   return (
     <>
-      {[...Array(3)].map((_, index: number) => (
-        <div
-          className="rounded-md shadow-sm inline-block sm:w-96 p-3 border-2 flex-1"
-          key={index}
-        >
-          <h2>
-            {"Account Type: "}
-            {account[index % account.length].type}
-          </h2>
-          <p>
-            {"Account Number: "}
-            {Number(account[index % account.length].number)}
-          </p>
-          <p>
-            {"Account Balance: "}
-            {changeToCurrency(Number(account[index % account.length].balance))}
-          </p>
+      {account.length ? (
+        [...Array(3)].map((_, index: number) => (
+          <div
+            className="rounded-md shadow-sm inline-block sm:w-96 p-3 border-2 flex-1"
+            key={index}
+          >
+            <h2>
+              {"Account Type: "}
+              {account[index % account.length]?.type}
+            </h2>
+            <p>
+              {"Account Number: "}
+              {Number(account[index % account.length]?.number)}
+            </p>
+            <p>
+              {"Account Balance: "}
+              {changeToCurrency(
+                Number(account[index % account.length]?.balance)
+              )}
+            </p>
+          </div>
+        ))
+      ) : (
+        <div className="rounded-md shadow-sm inline-block sm:w-96 p-3 border-2 flex-1 text-center flex h-full items-center justify-center">
+          <p>no data</p>
         </div>
-      ))}
+      )}
     </>
   );
 }
