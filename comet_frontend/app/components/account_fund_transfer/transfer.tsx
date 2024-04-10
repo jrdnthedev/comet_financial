d86e50dfe5f450df71c0d21e0bc7bfc492fa6540
smooooky <75350537+smooooky@users.noreply.github.com>
2024-04-10 00:05:47 -0400

    refactor: added deposit functionality

--- comet_frontend/app/components/account_fund_transfer/transfer.tsx
+++ comet_frontend/app/components/account_fund_transfer/transfer.tsx
@@ -1,5 +1,6 @@
 import { Account, AccountProps } from "@/app/interfaces/account/account";
 import {
+  depositFunds,
   transferToAccount,
   withdrawFromAccount,
 } from "@/app/lib/features/user/userSlice";
@@ -9,8 +10,11 @@ import { useState } from "react";
 
 export default function AccountTransfer(props: AccountProps) {
   const [amountToTransfer, setAmountToTransfer] = useState(0);
+  const [amountToDeposit, setAmountToDeposit] = useState(0);
   const [accountToTransferTo, setAccountToTransferTo] = useState(0);
   const [accountToTransferFrom, setAccountToTransferFrom] = useState(0);
+  const [accountToDepositTo, setAccountToDepositTo] = useState(0);
+  const [activeTab, setActiveTab] = useState(0);
   const dispatch = useAppDispatch();
 
   const handleTransfer = () => {
@@ -30,6 +34,29 @@ export default function AccountTransfer(props: AccountProps) {
     );
   };
 
+  const handleDeposit = () => {
+    console.log({
+      amount: amountToDeposit,
+      id: accountToDepositTo,
+      transferType: TransactionType.Deposit,
+    });
+    dispatch(
+      depositFunds({
+        amount: amountToDeposit,
+        id: accountToDepositTo,
+        transferType: TransactionType.Deposit,
+      })
+    );
+  };
+
+  const handleSetDepositToId = (e: any) => {
+    setAccountToDepositTo(Number(e.target.value));
+  };
+
+  const handleTabClick = (index: number) => {
+    setActiveTab(index);
+  };
+
   const handleSetTransferToId = (e: any) => {
     setAccountToTransferTo(Number(e.target.value));
   };
@@ -38,54 +65,114 @@ export default function AccountTransfer(props: AccountProps) {
   };
   return (
     <div className="rounded-md shadow-sm inline-block sm:w-96 p-3 border-2">
-      <h2 className="mb-6">Quick Transfer</h2>
-      <div className="mb-5">
-        <label htmlFor="fromAccount">From Account:</label>
-        <select
-          name="transferFromAccount"
-          id="fromAccount"
-          className="border-2 w-full inline-block  border-emerald-200 p-1 rounded-md"
-          onChange={handleSetTransferFromId}
-        >
-          {props.data.map((account: Account, index: number) => (
-            <option value={account.id} key={index}>
-              {account.type}
-            </option>
-          ))}
-        </select>
-      </div>
-      <div className="mb-5">
-        <label htmlFor="toAccount">To Account:</label>
-
-        <select
-          name="transferToAccount"
-          id="toAccount"
-          className="border-2 w-full inline-block border-emerald-200 p-1 rounded-md"
-          onChange={handleSetTransferToId}
-        >
-          {props.data.map((account: Account, index: number) => (
-            <option value={account.id} key={index}>
-              {account.type}
-            </option>
-          ))}
-        </select>
-      </div>
-      <div className="mb-5">
-        <input
-          type="text"
-          value={amountToTransfer}
-          className="border-2 w-full inline-block  border-emerald-200 p-1 rounded-md"
-          onChange={(e: any) => setAmountToTransfer(e.target.value)}
-        />
-      </div>
-      <div>
+      <div className="flex mb-3">
         <button
-          onClick={handleTransfer}
-          className="p-3 w-full bg-emerald-100 rounded-md"
+          onClick={() => handleTabClick(0)}
+          className={`cursor-pointer px-4 py-2 flex items-center justify-center flex-1 ${
+            activeTab === 0 ? "bg-gray-200" : ""
+          }`}
         >
-          transfer
+          <h2>Quick Transfer</h2>
+        </button>
+        <button
+          onClick={() => handleTabClick(1)}
+          className={`cursor-pointer px-4 py-2 flex items-center justify-center flex-1 ${
+            activeTab === 1 ? "bg-gray-200" : ""
+          }`}
+        >
+          <h2>Deposit</h2>
         </button>
       </div>
+      {activeTab === 0 ? (
+        <div className="mb-5">
+          <div className="mb-5">
+            <label htmlFor="fromAccount">From Account:</label>
+            <select
+              name="transferFromAccount"
+              id="fromAccount"
+              className="border-2 w-full inline-block  border-emerald-200 p-1 rounded-md"
+              onChange={handleSetTransferFromId}
+            >
+              {props.data.map((account: Account, index: number) => (
+                <option value={account.id} key={index}>
+                  {account.type}
+                </option>
+              ))}
+            </select>
+          </div>
+          <div className="mb-5">
+            <label htmlFor="toAccount">To Account:</label>
+
+            <select
+              name="transferToAccount"
+              id="toAccount"
+              className="border-2 w-full inline-block border-emerald-200 p-1 rounded-md"
+              onChange={handleSetTransferToId}
+            >
+              {props.data.map((account: Account, index: number) => (
+                <option value={account.id} key={index}>
+                  {account.type}
+                </option>
+              ))}
+            </select>
+          </div>
+          <div>
+            <input
+              type="text"
+              value={amountToTransfer}
+              className="border-2 w-full inline-block  border-emerald-200 p-1 rounded-md"
+              onChange={(e: any) => setAmountToTransfer(e.target.value)}
+            />
+          </div>
+        </div>
+      ) : (
+        <div className="mb-5">
+          <div className="mb-5">
+            <label htmlFor="depositToAccount">To Account:</label>
+
+            <select
+              name="depositToAccount"
+              id="depositToAccount"
+              className="border-2 w-full inline-block border-emerald-200 p-1 rounded-md"
+              onChange={handleSetDepositToId}
+            >
+              {props.data.map((account: Account, index: number) => (
+                <option value={account.id} key={index}>
+                  {account.type}
+                </option>
+              ))}
+            </select>
+          </div>
+          <div>
+            <input
+              type="text"
+              value={amountToDeposit}
+              className="border-2 w-full inline-block  border-emerald-200 p-1 rounded-md"
+              onChange={(e: any) => setAmountToDeposit(e.target.value)}
+            />
+          </div>
+        </div>
+      )}
+
+      {activeTab === 0 ? (
+        <div>
+          <button
+            onClick={handleTransfer}
+            className="p-3 w-full bg-emerald-100 rounded-md"
+          >
+            transfer
+          </button>
+        </div>
+      ) : (
+        <div>
+          <button
+            onClick={handleDeposit}
+            className="p-3 w-full bg-emerald-100 rounded-md"
+          >
+            deposit
+          </button>
+        </div>
+      )}
     </div>
   );
 }
